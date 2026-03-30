f284f9adf99f80415503f7d7b8651f04195ba416
Anton <adobonii@qualium-systems.com>
2026-03-30 16:17:06 +0300

    WIP Cleaning minigame

--- Assets/TheNeighbor/Scripts/GamePlay/MiniGame/CleaningMinigame/CleaningMiniGame.cs
+++ Assets/TheNeighbor/Scripts/GamePlay/MiniGame/CleaningMinigame/CleaningMiniGame.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Trellcko.Core.Input;
 using Trellcko.Gameplay.MiniGame;
 using Unity.Cinemachine;
@@ -10,12 +12,19 @@ public class CleaningMiniGame : MonoBehaviour, IMiniGame
     [SerializeField] private CinemachineCamera _cinemachineCamera;
     [SerializeField] private MopController _mopController;
     [SerializeField] private Texture2D _maskTexture;
+    [SerializeField] private Texture2D _originalTexture;
     
-    private ICursorController _cursorController;
-    public bool IsPlaying { get; private set; }
     public MiniGameType MinigameType => MiniGameType.CleaningMiniGame;
+    public bool IsPlaying { get; private set; }
+
+    private readonly List<Vector2Int> _coloredPixes = new();
+    private ICursorController _cursorController;
+    
     public event Action<bool, IMiniGame> Finished;
 
+    private const int TextureSize = 256;
+    private const float PercentToFinishGame = 0.95f;
+    
     [Inject]
     private void Construct(ICursorController cursorController)
     {
@@ -24,24 +33,54 @@ public class CleaningMiniGame : MonoBehaviour, IMiniGame
     
     public void StartGame()
     {
-        ClearTexture();
+        InitTexture();
 
         _cinemachineCamera.enabled = true;
         IsPlaying = true;
         _cursorController.UnlockCursor();
         _mopController.gameObject.SetActive(true);
+        _mopController.MaskUpdated += OnMaskUpdated;
     }
 
-    private void ClearTexture()
+    private void OnMaskUpdated()
     {
-        Color[] fill = new Color[256 * 256];
-        for(int i=0; i<fill.Length; i++) fill[i] = Color.white;
+        int clearPoints = _coloredPixes.Count(t => _maskTexture.GetPixel(t.x, t.y) == Color.black);
+
+        float percent = (float)clearPoints/ _coloredPixes.Count;
+        if (percent > PercentToFinishGame)
+        {
+            Finished?.Invoke(true, this);
+        }
+    }
+
+    private void InitTexture()
+    {
+        Color[] fill = new Color[TextureSize * TextureSize];
+        _coloredPixes.Clear();
+        
+        for(int i=0; i<fill.Length; i++) 
+            fill[i] = Color.white;
+        
         _maskTexture.SetPixels(fill);
+
+        for (int x = 0; x < TextureSize; x++)
+        {
+            for (int y = 0; y < TextureSize; y++)
+            {
+                if(_originalTexture.GetPixel(x, y) != Color.clear)
+                    _coloredPixes.Add(new(x, y));
+            }
+        }
+        
+        Debug.LogError(_coloredPixes.Count);
+        
         _maskTexture.Apply();
+        
     }
 
     public void FinishGame(bool success)
     {
+        _mopController.MaskUpdated -= OnMaskUpdated;
         Finished?.Invoke(success, this);
     }
 
--- Assets/TheNeighbor/Scripts/GamePlay/MiniGame/CleaningMinigame/MopController.cs
+++ Assets/TheNeighbor/Scripts/GamePlay/MiniGame/CleaningMinigame/MopController.cs
@@ -7,16 +7,25 @@ namespace Trellcko.Gameplay.MiniGame
 {
     public class MopController : MonoBehaviour
     {
+        private const float Step = 0.0390f; // half brush Size / 256
         [SerializeField] private MeshRenderer _target;
         [SerializeField] private Transform _mopPoint;
         [SerializeField] private Texture2D _maskTexture;
         [SerializeField] private float _sensitivity;
+        [SerializeField] private int _brushSize = 100;
+
+        public event Action MaskUpdated;
         
         private IInputHandler _inputHandler;
+        private Vector3 _lastMopPosition;
+
+        private void Start()
+        {
+            _lastMopPosition = _mopPoint.position;
+        }
 
         private Camera _camera;
         private Vector3 _startPosition;
-        [SerializeField] private int _brushSize = 100;
 
         private void OnDrawGizmos()
         {
@@ -42,16 +51,36 @@ namespace Trellcko.Gameplay.MiniGame
 
         private void Update()
         {
-            MoveMop();
-            PaintMaskAt(_mopPoint.position);
+            if (MoveMop())
+            {
+                PaintLine(_lastMopPosition, _mopPoint.position);
+                MaskUpdated?.Invoke();
+                _lastMopPosition = _mopPoint.position;
+            }
         }
 
-        private void MoveMop()
+        private void PaintLine(Vector3 lastMopPosition, Vector3 mopPointPosition)
+        {
+            float distance = Vector3.Distance(mopPointPosition, lastMopPosition);
+
+            for (float t = 0; t <= 1f; t += Step / distance)
+            {
+                Vector3 point = Vector3.Lerp(lastMopPosition, mopPointPosition, t);
+                PaintMaskAt(point);
+            }
+        }
+
+        private bool MoveMop()
         {
             Vector3 mouseDelta = _inputHandler.GetMouseDelta();
+
+            if (mouseDelta.magnitude == 0)
+                return false;
+            
             mouseDelta *= _sensitivity * Time.deltaTime;
 
             transform.position += mouseDelta;
+            return true;
         }
 
         private void PaintMaskAt(Vector3 worldPos)
@@ -69,10 +98,16 @@ namespace Trellcko.Gameplay.MiniGame
             for (int x = -halfBrush; x < halfBrush; x++)
             for (int y = -halfBrush; y < halfBrush; y++)
             {
+                if (x * x + y * y > _brushSize)
+                    continue;
+
                 int tx = px + x;
                 int ty = py + y;
+
                 if (tx >= 0 && tx < _maskTexture.width && ty >= 0 && ty < _maskTexture.height)
+                {
                     _maskTexture.SetPixel(tx, ty, Color.black);
+                }
             }
 
             _maskTexture.Apply();
