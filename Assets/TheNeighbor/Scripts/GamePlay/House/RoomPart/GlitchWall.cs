f7fa42647089c63bf022c9fccf1f3ed5687c380d
Anton <doboniy03@gmail.com>
2026-05-28 14:59:42 +0300

    Wall Update

--- Assets/TheNeighbor/Scripts/GamePlay/House/RoomPart/GlitchWall.cs
+++ Assets/TheNeighbor/Scripts/GamePlay/House/RoomPart/GlitchWall.cs
@@ -5,7 +5,7 @@ namespace Trellcko.Gameplay.House
     public class GlitchWall : MonoBehaviour
     {
         [SerializeField] private MeshRenderer[] _renderers;
-
+        
         public void SetRendererLayerMask(uint mask)
         {
             foreach (MeshRenderer renderer in _renderers)
---
+++ Assets/TheNeighbor/Scripts/GamePlay/House/RoomPart/WallMouthController.cs
@@ -0,0 +1,39 @@
+using System;
+using CastleWarriors.GameLogic.Utils;
+using UnityEngine;
+
+namespace Trellcko.Gameplay.House
+{
+    public class MouthController : MonoBehaviour
+    {
+        [SerializeField] private MeshRenderer _renderer;
+
+        [SerializeField] private Texture _openMouth;
+        [SerializeField] private Texture _closeMouth;
+        
+        [SerializeField] private Vector2 _timeToChangeMouthStateMinMax;
+
+        private BetterTimer _timer;
+
+        private bool _isOpened;
+
+        private void Awake()
+        {
+            _timer = new(_timeToChangeMouthStateMinMax, true, true);
+            _timer.Completed += OnCompleted;
+            _timer.Reset();
+        }
+
+        private void Update()
+        {
+            _timer.Tick();
+        }
+
+        private void OnCompleted()
+        {
+            Texture texture = _isOpened ? _closeMouth : _openMouth;
+            _renderer.material.mainTexture = texture;
+            _isOpened = !_isOpened;
+        }
+    }
+}
